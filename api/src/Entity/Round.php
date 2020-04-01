3f46221a40cc07d8bebc085e34b0cf7bd50f7d08
RafalMalik <rafalmalik.info@gmail.com>
2020-04-01 23:17:04 +0200

    Created round object prototype.

---
+++ api/src/Entity/Round.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ApiResource()
+ * @ORM\Entity(repositoryClass="App\Repository\RoundRepository")
+ */
+class Round
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $status;
+
+    /**
+     * @ORM\Column(type="json")
+     */
+    private $results = [];
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getStatus(): ?int
+    {
+        return $this->status;
+    }
+
+    public function setStatus(int $status): self
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+
+    public function getResults(): ?array
+    {
+        return $this->results;
+    }
+
+    public function setResults(array $results): self
+    {
+        $this->results = $results;
+
+        return $this;
+    }
+}
