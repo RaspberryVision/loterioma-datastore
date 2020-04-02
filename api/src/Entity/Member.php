922d6ea22abb2a13313481ea68891f26a363e894
RafalMalik <rafalmalik.info@gmail.com>
2020-04-02 14:09:19 +0200

    Added member entity.

--- api/src/Entity/Member.php
+++ api/src/Entity/Member.php
@@ -7,9 +7,9 @@ use Doctrine\ORM\Mapping as ORM;
 
 /**
  * @ApiResource()
- * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
+ * @ORM\Entity(repositoryClass="App\Repository\MemberRepository")
  */
-class User
+class Member
 {
     /**
      * @ORM\Id()
@@ -27,16 +27,6 @@ class User
      * @ORM\Column(type="string", length=255)
      */
     private $password;
-
-    /**
-     * @ORM\Column(type="string", length=255)
-     */
-    private $role;
-
-    /**
-     * @ORM\Column(type="string", length=255)
-     */
-    private $securityToken;
 
     public function getId(): ?int
     {
@@ -63,30 +53,6 @@ class User
     public function setPassword(string $password): self
     {
         $this->password = $password;
-
-        return $this;
-    }
-
-    public function getRole(): ?string
-    {
-        return $this->role;
-    }
-
-    public function setRole(string $role): self
-    {
-        $this->role = $role;
-
-        return $this;
-    }
-
-    public function getSecurityToken(): ?string
-    {
-        return $this->securityToken;
-    }
-
-    public function setSecurityToken(string $securityToken): self
-    {
-        $this->securityToken = $securityToken;
 
         return $this;
     }
